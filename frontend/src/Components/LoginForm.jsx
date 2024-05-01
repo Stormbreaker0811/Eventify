72baa5f55116ce284a39787d1dc817adcaa1aae4
Stormbreaker0811 <nimishgodbole409@gmail.com>
2024-05-01 12:27:53 +0530

    Login css half done and added material ui menu for navbar

--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -1,5 +1,7 @@
-import React,{ useState,useContext } from 'react';
-import Lottie, { LottiePlayer } from 'lottie-react'
+import React,{ useState } from 'react';
+import Lottie, { LottiePlayer } from 'lottie-react';
+import { SlLogin } from "react-icons/sl";
+import { Button } from '@mui/material';
 import axios from 'axios';
 import PhoneInput from 'react-phone-input-2';
 import "react-phone-input-2/lib/style.css";
@@ -10,22 +12,21 @@ import animation from '../Assets/Lottie Assets/Login.json'
 const LoginForm = () => {
     const baseUrl = "http://localhost:5000";
     axios.defaults.baseURL = baseUrl;
-    
-    const emailOrMobileNo = (state) => {
-        if(state === "email"){
-            return(
-                <input type="email" name="email" id="" placeholder='Enter Email: ' onChange={(e) => setEmail(e.target.value)} />
-            );
-        }else{
-            return(
-                <PhoneInput
-                className="number"
-                country={"in"}
-                value={this.state.phone} />
-            )
-        }
-    };
 
+    const [showEmail,setShowEmail] = useState(true);
+    const [showMobile,setShowMobile] = useState(false);
+
+    const toggleEmail = (e) => {
+        e.preventDefault()
+        setShowEmail(true);
+        setShowMobile(false);
+    }
+    
+    const toggleMobile = (e) => {
+        e.preventDefault()
+        setShowEmail(false);
+        setShowMobile(true);
+    }
 
     const [password, setPassword] = useState('');
     const [email,setEmail] = useState('');
@@ -44,17 +45,41 @@ const LoginForm = () => {
     // const [gender,setGender] = useState('');
 
     const handleSubmit = (e) => {
+        e.preventDefault();
+        if(showEmail === true){
+            setFormState({
+                email: email,
+                password: password
+            })
+        }else if(showMobile === true){
+            setFormState({
+                mobile: mobileNo,
+                password: password
+            })
+        }
+    }
 
+    const handleChange = (e) => {
+        e.preventDefault();
     }
 
     return (
     <div className='container'>
-        <h1>Login</h1>
-        <form method="post" onSubmit={handleSubmit}>
-            Email<input type="checkbox" name="" id="" />Mobile
-            <br /><input type="text" name="email/mobile" id="" placeholder='Enter Password: '/>
-            <br />
-        </form>
+        <div className='lottie-container'>
+            <Lottie 
+            animationData={animation}/>
+        </div>
+        <div className="login-container">
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit} onChange={handleChange}>
+                <button onClick={toggleEmail} className='toggleEmail'>Login with Email</button>
+                <button onClick={toggleMobile} className='toggleMobile'>Login with Mobile</button>
+                <br />{showEmail && <input type='email' placeholder='Enter Email' onChange={(e) => setEmail(e.target.value)}/>}
+                <br />{showMobile && <PhoneInput country={'in'} placeholder='Enter Mobile Number ' value='' onChange={(value) => setMobileNo(value)} />}
+                <br /><input type="password" placeholder='Enter Password: ' className='pass' onChange={(e) => setPassword(e.target.value)}/>
+                <br /><Button variant='contained' >Login</Button>
+            </form>
+        </div>
     </div>
     )
 }
